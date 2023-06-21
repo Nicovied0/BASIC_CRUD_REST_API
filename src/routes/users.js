cc3a5c30942f2d69f22d0be9d882610549ca4c47
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-21 00:13:56 -0300

    sadkasds

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
+// const axios = require("axios");
+import axios from 'axios'
 
 const router = new Router();
 
 router.get("/", async (req, res) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  res.json(data);
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching data" });
+  }
 });
 
 export default router;
