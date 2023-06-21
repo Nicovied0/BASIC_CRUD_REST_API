34a69a7e61ce141747db4b375a314dc6ca4d14d1
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-21 00:39:45 -0300

    ´kaskdwds

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,9 @@ const router = new Router();
 
 router.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
     const data = response.data;
     res.json(data);
   } catch (error) {
