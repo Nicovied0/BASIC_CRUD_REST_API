5d9d993f133458ce7e10d808315e94156d02a6fc
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-21 00:17:15 -0300

    addsasd

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = new Router();
 
-router.get("/test", (req, res) => {
+router.get("/", (req, res) => {
   const data = {
     name: "Nico",
     website: "ondev.vercel.app",
